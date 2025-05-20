240d12277accba08e140b391230d15b83154c181
abhijitWebDev <110275876+abhijitWebDev@users.noreply.github.com>
2025-05-20 11:50:08 +0530

    edited in config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,13 +5,4 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-  proxy: {
-    '/ingest': 'http://localhost:8000',
-    '/detect': 'http://localhost:8000',
-    '/hazards': 'http://localhost:8000',
-    '/trends': 'http://localhost:8000',
-    '/export': 'http://localhost:8000'
-  }
-}
 })
